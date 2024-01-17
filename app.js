a13973cfabe59a1463eb8433ee942a8da60b2a9b
Taylor <96550502+taylors42@users.noreply.github.com>
2024-01-16 23:17:39 -0300

    .

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 const http = require('http');
-const fs = require('fs')
-function requestListener(request, response)
-  {
-    const url = request.url;
-    const method = request.method;
-    if ( url === '/')
-      {
-        response.setHeader('Content-type', 'text-html')
-        response.write('<html>')
-        response.write('<head><title>Enter message</title></head>')
-        response.write('<body><form action="/message" method="POST" name="message"><input type="text"><button type="submit">Submit</button></input></form></body>')
-        response.write('</html>')
-        response.end();
-      }
-    else if ( url === '/message' && method === 'POST')
-      {
-        fs.writeFileSync('message.txt', 'random text');
-        response.statusCode = 302;
-        response.setHeader('location', '/');
-        return response.end
 
-      }
-    response.setHeader('Content-type', 'text-html')
-    response.write('<html>')
-    response.write('<head><title>my first page</title></head>')
-    response.write('<body>Thats a return from a http server</body>')
-    response.write('</html>')
-    response.end();
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Assignment 1</title></head>');
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
   }
-const server = http.createServer(requestListener);
-// http.createServer(function(resquest, response)
-//   {
-  //     console.log("a anonymous function with nothing")
-  //   });
-  // http.createServer((request, response) => 
-  // {
-  // });
+  if (url === '/users') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Assignment 1</title></head>');
+    res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  // Send a HTML response with some "Page not found text
+  if (url === '/create-user') {
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody.split('=')[1]); // username=whatever-the-user-entered
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    res.end();
+  }
+});
+
 server.listen(3000);
